ce9292be3730ee7559758932e79c4fb0c4ff4924
kevinyohe <kevinyohe7@yahoo.com>
2020-12-20 20:45:50 -0500

    add toc

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,18 @@ import {graphql} from 'gatsby';
 import Img from 'gatsby-image';
 
 const Index = ({data}) =>{
+    console.log(data.toc.nodes[0].frontmatter)
     return (
         <div>
             <h1>Frozen Code</h1>
             <Img fluid={data.file.childImageSharp.fluid} alt="frozen waterfall" />
             {/* <img src={data.file.childImageSharp.fixed.src} alt="frozen waterfall" style={{maxWidth: 800}} /> */}
+        
+        <ul>
+        {data.toc.nodes.map((thing)=> (
+        <li style={{listStyleType: "none"}}><a href={thing.frontmatter.slug}>{thing.frontmatter.title}</a></li>
+        ))}
+        </ul>
         </div>
 
     )
@@ -26,7 +33,16 @@ query HomepageQuery {
         }
       }
     }
+    toc: allMarkdownRemark {
+    nodes {
+      frontmatter {
+        slug
+        title
+        description
+      }
+    }
   }
+}
 `
 
 
